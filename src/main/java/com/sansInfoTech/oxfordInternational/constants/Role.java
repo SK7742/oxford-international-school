13159c4c8f1d5ef4d604c1679c4f4e7d926e558c
Shivam Kumar <shivamkumargupta07@gmail.com>
2025-09-28 18:11:55 +0530

    UI integrations

---
+++ src/main/java/com/sansInfoTech/oxfordInternational/constants/Role.java
@@ -0,0 +1,8 @@
+package com.sansInfoTech.oxfordInternational.constants;
+
+public enum Role {
+    SUPERUSER,
+    ADMIN,
+    TEACHER,
+    STUDENT
+}
---
+++ src/main/java/com/sansInfoTech/oxfordInternational/responseDTO/AuthResponseDTO.java
@@ -0,0 +1,11 @@
+package com.sansInfoTech.oxfordInternational.responseDTO;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDTO {
+    private String username;
+    private String role;
+    private String message;
+
+}
